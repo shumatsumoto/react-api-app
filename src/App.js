44d0210362c72768bc2e0a86f5f557f106dc8bb4
Shu Matsumoto <shumatsumoto0328@gmail.com>
2024-04-28 21:29:47 +0900

    axios:data fetch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import { useEffect } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  useEffect(() => {
+    fetchCharacters();
+  }, []);
+
+  const fetchCharacters = async () => {
+    const apiUrl = "https://narutodb.xyz/api/character";
+    const result = await axios.get(apiUrl);
+    console.log(result.data.characters);
+  };
   return <div className="App">Hello!!!</div>;
 }
 
