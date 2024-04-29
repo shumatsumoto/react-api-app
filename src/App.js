58170ff87a6e30966d0efb1f0fdbed89317bd722
Shu Matsumoto <shumatsumoto0328@gmail.com>
2024-04-29 12:10:41 +0900

    データ表示

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
 
 function App() {
+  const [characters, setCharacters] = useState([]);
   useEffect(() => {
     fetchCharacters();
   }, []);
@@ -10,9 +11,31 @@ function App() {
   const fetchCharacters = async () => {
     const apiUrl = "https://narutodb.xyz/api/character";
     const result = await axios.get(apiUrl);
-    console.log(result.data.characters);
+    setCharacters(result.data.characters);
+    console.log(result);
   };
-  return <div className="App">Hello!!!</div>;
+  return (
+    <div className="container">
+      <main>
+        <div className="card-container">
+          {characters.map((character, index) => (
+            <div className="card" key={index}>
+              <div className="card-image">
+                <img
+                  src={character.images[0] || "dummy.png"}
+                  alt={character.name}
+                />
+              </div>
+              <div className="card-content">
+                <h2>{character.name}</h2>
+                <p>{character.description}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default App;
